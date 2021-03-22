bd3309bf5ae383da4b9ef7c302f756aff87dd4c0
Oleh Samoilenko <oilsamoilenko@gmail.com>
2021-03-22 14:10:36 +0200

    L2: v.1
    Signed-off-by: Oleh Samoilenko <oilsamoilenko@gmail.com>

--- L2/Board.py
+++ L2/Board.py
@@ -1,4 +1,3 @@
-import time
 import random
 import math
 
@@ -7,41 +6,38 @@ class Board(object):
 
     def __init__(self, queens):
         """Instances differ by their queen placements."""
-        self.queens = queens.copy() # No aliasing!
+        self._queens = queens.copy()
+
 
     def display(self):
         """Print the board."""
-        for r in range(len(self.queens)):
-            for c in range(len(self.queens)):
-                if self.queens[c] == r:
+        for r in range(len(self._queens)):
+            for c in range(len(self._queens)):
+                if self._queens[c] == r:
                     print('Q ', end=''),
                 else:
                     print('- ', end=''),
             print()
         print()
-    
-    
-    def moves(self):
-        """Return a list of possible moves given the current placements.""" 
-        
+
 
     def neighbor(self):
-        """Return a Board instance like this one but with one move made.""" # TODO: rename
-        tmp_queens = self.queens.copy()
-        random_col = random.choice(range(8))
-        random_row = random.choice(range(8))
+        """Return a Board instance like this one but with one move made."""
+        tmp_queens = self._queens.copy()
+        random_col = random.choice(range(len(self._queens)))
+        random_row = random.choice(range(len(self._queens)))
         tmp_queens[random_col] = random_row
         return Board(tmp_queens)
+
 
     def cost(self) -> int:
         """Compute the cost of this solution."""
         number_of_conflicts = 0
 
         # increment cost if two queens are in same row or in same diagonal.
-        for i in range(0, len(self.queens)):
-            for j in range(i + 1, len(self.queens)):
-                if self.queens[i] == self.queens[j] or abs(self.queens[i] - self.queens[j]) == j - i:
-                    # print(i, j)
+        for i in range(0, len(self._queens)):
+            for j in range(i + 1, len(self._queens)):
+                if self._queens[i] == self._queens[j] or abs(self._queens[i] - self._queens[j]) == j - i:
                     number_of_conflicts += 1
 
         return number_of_conflicts
@@ -49,67 +45,62 @@ class Board(object):
 class Agent(object):
     """Knows how to solve an n-queens problem with simulated annealing."""
 
-    def anneal(self, board):
-        """Return a list of moves to adjust queen placements."""
-        # YOU FILL THIS IN
+    def solve(self, board: Board):
+        j = 0
+        i = 0
+        while self._t > self._t_stop:
+            i += 1
+            print("Iteration " + str(i))
+            
+            neighbor = board.neighbor()
+            board_cost = board.cost()
+            neighbor_cost = neighbor.cost()
 
-def main():
-    """Create a problem, solve it with simulated anealing, and console-animate."""
-
-    queens = {0: 3, 1: 0, 2: 2, 3: 6, 4: 4, 5: 3, 6: 3, 7: 7}#dict()
-    # for col in range(8):
-    #     row = random.choice(range(8))
-    #     queens[col] = row
-    # print(queens)
-
-    board = Board(queens)
-    # board.display()
-
-    t = 30
-    decay_rate = 0.98
-    
-    # agent = Agent()
-    # path = agent.anneal(board)
-    
-    print("Initial board:")
-    board.display()
-    # while path:
-    # move = path.pop(0)
-    
-    for i in range(1000):
-        print("Iteration " + str(i))
-        neighbor = board.neighbor()
-        # neighbor.display()
-
-        board_cost = board.cost()
-        neighbor_cost = neighbor.cost()
-        # print(board.cost())
-        # print(neighbor.cost())
-
-        if neighbor_cost < board_cost:
-            board = neighbor
-            print("updated board, cost: %d (lower)" % neighbor_cost)
-            board.display()
-            if neighbor_cost == 0:
-                print("DONE!")
-                break
-        else:
-            c = neighbor_cost - board_cost
-            p = math.exp(-c / t)
-            # print(p)
-            rnd = random.random()
-            # print(rnd)
-            if rnd < p:
+            if neighbor_cost < board_cost:
                 board = neighbor
-                print("updated board, cost: %d (greater, t = %f)" % (neighbor_cost, t))
+                print("updated board, cost: %d (lower)" % neighbor_cost)
                 board.display()
+                if neighbor_cost == 0:
+                    print("DONE!")
+                    break
+            else:
+                c = neighbor_cost - board_cost
+                p = math.exp(-c / self._t)
+                rnd = random.random()
+                if rnd < p:
+                    board = neighbor
+                    print("updated board, cost: %d (greater, t = %f)" % (neighbor_cost, self._t))
+                    board.display()
 
-        # TODO: every 100 iterations
-        t *= decay_rate
+            j += 1
+            if j == self._iterations_to_decay:
+                self._t *= self._decay_rate
+                j = 0
 
-    # board = board.neighbor(move)
-    # time.sleep(0.1)
-    # board.display()
+        print("Final temperature: %f" % self._t)
+
+
+    def __init__(self, t=30.0, t_stop=0.5, decay_rate=0.98, iterations_to_decay=100):
+        self._t = t
+        self._t_stop = t_stop
+        self._decay_rate = decay_rate
+        self._iterations_to_decay = iterations_to_decay
+
+
+def generate_queens(n: int) -> dict:
+    queens = dict()
+    for col in range(n):
+        row = random.choice(range(n))
+        queens[col] = row
+    return queens
+
 
 if __name__ == '__main__':
-    main()
+    # queens = {0: 3, 1: 0, 2: 2, 3: 6, 4: 4, 5: 3, 6: 3, 7: 7}
+    queens = generate_queens(8)
+    board = Board(queens)
+    print("Initial board:")
+    board.display()
+    
+    agent = Agent(t=30.0, t_stop=0.5, decay_rate=0.98, iterations_to_decay=100)
+    agent.solve(board)
